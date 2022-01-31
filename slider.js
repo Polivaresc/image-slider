a85697a9d526a0d293a600c64191b6a9e832f125
Polivaresc <paulaolca98@gmail.com>
2022-01-31 18:50:05 +0100

    start slider

---
+++ slider.js
@@ -0,0 +1,15 @@
+
+const nextButton = document.querySelector('#next')
+const previousButton = document.querySelector('#previous')
+
+nextButton.addEventListener('click', nextSlide)
+
+previousButton.addEventListener('click', previousSlide)
+
+function nextSlide() {
+
+}
+
+function previousSlide() {
+
+}
